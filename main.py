b2584c357d55d8c984e5b9132d43c75d78614cb4
Gokul Sankar <gokul.sankar29@gmail.com>
2026-05-15 21:35:06 +0530

    Abstracted prime sweeping code

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import solvers
 import polynomials
 
 system = [[(2, {"x" : 2}), (-1, {"y" : 1}), (-1, {})], [(1, {"y" : 1})]]
-print(f"Printing system\n{polynomials.system_to_string(system)}\nSystem Done")
+print("Printing system")
+print(polynomials.system_to_string(system))
+print("System Done")
 
 is_sat = solvers.check_sat_using_smt_reals(system)
 print(f"is_sat: {is_sat}")
 
-is_sat = solvers.check_sat_by_sweeping_primes(system)
+is_sat = solvers.check_sat_by_sweeping_primes_z3(system)
 print(f"is_sat: {is_sat}")
--- solvers.py
+++ solvers.py
@@ -1,5 +1,6 @@
 import enum
 import random
+from typing import Callable
 
 import z3
 
@@ -13,6 +14,15 @@ class SatResult(enum.Enum):
   SAT = 0
   UNSAT = 1
   UNKNOWN = 2
+
+  @staticmethod
+  def from_z3_sat_result(sat_result : z3.CheckSatResult) -> SatResult:
+    if sat_result == z3.sat:
+      return SatResult.SAT
+    elif sat_result == z3.unsat:
+      return SatResult.UNSAT
+    else:
+      return SatResult.UNKNOWN
 
 def convert_equation_to_z3_expr_using_reals(eqn : Equation) -> z3.ExprRef:
   eqn_expr = None
@@ -33,15 +43,14 @@ def convert_system_to_z3_expr_using_reals(sys : System) -> z3.ExprRef:
     exprs_for_eqns.append(convert_equation_to_z3_expr_using_reals(eqn))
   return z3.And(*exprs_for_eqns)
 
-def solve_using_smt_reals(sys : System) -> z3.CheckSatResult:
-  print("Checking satisfiablilty using SMT reals")
-  real_system, imag_system = polynomials.split_system_into_real_and_complex(sys)
-  final_system = [*real_system, *imag_system]
-  z3_expr = convert_system_to_z3_expr_using_reals(final_system)
-  s = z3.Solver()
-  s.add(z3_expr)
-  s.set("timeout", 60000)
-  return s.check()
+def generate_pow_z3_expr(z3_var : z3.ExprRef, exp : int, p : int) -> z3.ExprRef:
+  if exp < 1:
+    raise Exception("exp < 1 is not supported")
+
+  expr : z3.ExprRef = z3_var
+  for _ in range(exp - 1):
+    expr = (expr * z3_var) % p
+  return expr
 
 def convert_equation_to_z3_expr_using_int_mod_p(p : int, eqn : Equation) -> z3.ExprRef:
   eqn_expr : z3.ExprRef = None
@@ -49,7 +58,7 @@ def convert_equation_to_z3_expr_using_int_mod_p(p : int, eqn : Equation) -> z3.E
     term_expr = z3.IntVal(coeff % p)
     for var, exp in vars_dict.items():
       z3_var = z3.Int(var) % p
-      term_expr = (term_expr * z3.ToInt((z3_var ** z3.IntVal(exp)))) % p
+      term_expr = (term_expr * generate_pow_z3_expr(z3_var, exp, p)) % p
     if eqn_expr is None:
       eqn_expr = term_expr
     else:
@@ -65,34 +74,39 @@ def convert_system_to_z3_expr_using_int_mod_p(p: int, sys : System) -> z3.ExprRe
     exprs_for_eqns.append(convert_equation_to_z3_expr_using_int_mod_p(p, eqn))
   return z3.And(*exprs_for_eqns)
 
-def solve_using_smt_int_mod_p(p : int, sys : System) -> z3.CheckSatResult:
+def solve_using_smt_reals(sys : System) -> SatResult:
+  print("Checking satisfiablilty using SMT reals")
+  real_system, imag_system = polynomials.split_system_into_real_and_complex(sys)
+  final_system = [*real_system, *imag_system]
+  z3_expr = convert_system_to_z3_expr_using_reals(final_system)
+  s = z3.Solver()
+  s.add(z3_expr)
+  s.set("timeout", 60000)
+  return SatResult.from_z3_sat_result(s.check())
+
+def solve_using_z3_int_mod_p(p : int, sys : System) -> SatResult:
   z3_expr = convert_system_to_z3_expr_using_int_mod_p(p, sys)
   s = z3.Solver()
   s.add(z3_expr)
   s.set("timeout", 60000)
-  return s.check()
+  return SatResult.from_z3_sat_result(s.check())
+
 
 def check_sat_using_smt_reals(sys: System) -> SatResult:
-  is_sat = solve_using_smt_reals(sys)
-  if is_sat == z3.sat:
-    return SatResult.SAT
-  elif is_sat == z3.unsat:
-    return SatResult.UNSAT
-  else:
-    return SatResult.UNKNOWN
+  return solve_using_smt_reals(sys)
 
-def check_sat_by_sweeping_primes(sys: System) -> SatResult:
-  print(f"Checking satisfiablilty by sweeping primes using SMT ints")
+def check_sat_by_sweeping_primes(sys : System,
+                                 solve_fn : Callable[[int, System], SatResult]) -> SatResult:
   sampled_primes = random.sample(PRIMES, NUM_PRIMES_TO_SAMPLE)
   num_sat = 0
   had_timeout_or_unknown = False
   for prime in sampled_primes:
     print(f"Checking SAT for int mod {prime}")
-    is_sat = solve_using_smt_int_mod_p(prime, sys)
+    is_sat = solve_fn(prime, sys)
     print(f"{is_sat}")
-    if is_sat == z3.sat:
+    if is_sat == SatResult.SAT:
       num_sat += 1
-    elif is_sat == z3.unknown:
+    elif is_sat == SatResult.UNKNOWN:
       had_timeout_or_unknown = True
 
   ratio_of_sat = num_sat / NUM_PRIMES_TO_SAMPLE
@@ -106,3 +120,7 @@ def check_sat_by_sweeping_primes(sys: System) -> SatResult:
     return SatResult.UNSAT
 
   return SatResult.UNKNOWN
+
+def check_sat_by_sweeping_primes_z3(sys: System) -> SatResult:
+  print(f"Checking satisfiablilty by sweeping primes using Z3 ints")
+  return check_sat_by_sweeping_primes(sys, solve_using_z3_int_mod_p)
